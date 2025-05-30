598c896323a8b50895f6644285a5763f10acad74
Andrey2709 <57500032+Andrey2709@users.noreply.github.com>
2025-05-30 17:52:30 +0300

    Создан проект Интернет магазин(Бизнес) демонстрационная версия

---
+++ news-with-filter/.section.php
@@ -0,0 +1,6 @@
+<?
+$sSectionName = "Новости с фильтром";
+$arDirProperties = Array(
+
+);
+?>
---
+++ news-with-filter/index.php
@@ -0,0 +1,4 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Новости с фильтром");
+?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
